447c639be716be085e36a74cc357022e552f40ca
OLEARIFILIPPO <64217497+OLEARIFILIPPO@users.noreply.github.com>
2023-06-30 16:52:11 +0200

    card di errore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,36 @@ import { MenuComponent } from './components/menu/menu.component';
 import { MenubarModule } from 'primeng/menubar';
 import { MainCardComponent } from './components/main-card/main-card.component';
 import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatExpansionModule, } from '@angular/material/expansion';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MatDividerModule} from '@angular/material/divider';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
 import { HttpClientModule } from '@angular/common/http';
+ 
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    MainCardComponent
+    MainCardComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MenubarModule,
     CardModule,
-    HttpClientModule
+    HttpClientModule,
+    ButtonModule,
+    MatButtonModule,
+    MatCardModule,
+    MatExpansionModule,
+    NoopAnimationsModule,
+    MatDividerModule,
+    OverlayPanelModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/main-card/main-card.component.ts
+++ src/app/components/main-card/main-card.component.ts
@@ -13,16 +13,17 @@ export class MainCardComponent implements OnInit{
   private url: string = 'https://localhost:7086/api/ErroriCaldaie'
 
   public errori: ErroriCaldaie[] = []
-
+  public panelOpenState: boolean[] = [];
+  
   ngOnInit(){
     this.http.get<ErroriCaldaie[]>(this.url).subscribe((data=>{
       this.errori = data
       console.log(data);
       this.errori.forEach(e => {
-        console.log(e['codiceErrore']);
-        
+        //console.log(e['codiceErrore']);
+        this.panelOpenState.push(false)
       });
+      console.log(this.panelOpenState);
     }));
-
   }
 }
