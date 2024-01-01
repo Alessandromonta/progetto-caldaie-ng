6e6804da6e8121f6c419b5fea6b345d201a3d372
Alessandromonta <alessandro.monta0801@gmail.com>
2024-01-01 13:25:45 +0100

    Migrazione angular 16

--- src/app/Auth/Guard/auth.guard.ts
+++ src/app/Auth/Guard/auth.guard.ts
@@ -1,11 +1,11 @@
 //Qui verifico se l'utente è autenticato e se non lo è ritorna alla pagina di login
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(private router: Router) {}
 
   canActivate(): boolean {
